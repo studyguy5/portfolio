bb31e778948ef27bd3e2e28cf7206e470ea042dd
Leonhard Siegele <leonhard.siegele39@gmail.com>
2026-04-24 23:43:05 +0200

    start setting Up language-switch Object

---
+++ langpackage.js
@@ -0,0 +1,58 @@
+
+const translations = {
+    "About me": "Über mich",
+    "Skills": "Fähigkeiten",
+    "Projects": "Projekte",
+    "Frontend Developer": "Frontend Entwickler",
+    "Check my Work": "Meine Projekte",
+    "Contact me": "Kontakt",
+    "based in Austria": "Zuhause in Österreich",
+    "love coding": "liebe Coding",
+    "ready for Remote Work": "bereit für Remote Arbeit",
+    "ready to Learn": "bereit zur Weiterbildung",
+    "Who I am": "Wer ich bin",
+
+    [`Hey there, I'm Leonhard! I'm a frontend developer with a passion 
+    for good looking user-friendly interfaces, which simplifies user's lives 
+    and cuts unnecessary chaos. I love Coding reladet stuff, because I can 
+    practice my creativity, understanding systems do problem solving. 
+    Basicly I find solutions and improve them afterwords, like an architect or engineer.`]: 
+    [`Hey, ich bin Leonhard! Ich bin ein Frontend-Entwickler mit einer Leidenschaft 
+    für gut aussehende benutzerfreundliche Oberflächen, die das Leben der Benutzer 
+    vereinfachen und unnötiges Chaos beseitigen. Ich liebe Coding-bezogene Dinge, 
+    weil ich meine Kreativität ausleben und Systeme verstehen kann, um Probleme zu lösen.
+    Grundsätzlich finde ich Lösungen und verbessere sie danach, wie ein Architekt oder Ingenieur.`],
+
+    ["I'm currently based in Austria/Innsbruck, but I'm open for remote work or relocation with almmostno limit"]:
+    ["Ich bin derzeit in Osterreich/Innsbruck, aber ich bin offen auf Remote Arbeit oder Umzug mit fast keiner Begrenzung"],
+
+    [`Improving my Skills is standart in IT, but I'm seriously addicted to learning new things.
+    Especially in the AI Aria, I want to know how LLM's work and the list goes on but this container here is to small for that`]:
+    [`Die Verbesserung meiner Fähigkeiten ist in der IT Standard, aber ich bin ernsthaft süchtig danach, neue Dinge zu lernen.`],
+
+    [`The exiting part of my job as a Frontend Developer is problem-solving and finding solutions for various problems.
+    My approach is mostly look for at least two perspektives to solve a problem and decide which has a good afect/komplexity ratio.`]:
+    [`Der spannende Teil meiner Arbeit als Frontend-Entwickler ist die Problemlösung und das Finden von Lösungen für verschiedene Probleme. 
+    Mein Ansatz besteht darin, nach mindestens zwei Perspektiven zu suchen, um ein Problem zu lösen, und zu entscheiden, 
+    welche ein gutes Verhältnis von Effektivität zu Komplexität hat.`],
+
+    "Technologies": "Technologien",
+    "Skill Set": "Skill Set",
+    
+    [`I have created a good fundament <br>of Skills in HTML, CSS and JavaScript through my project focused work.
+    I made a Task-Manager, a Canvas Game and a few easy projects where I started. My takeaways are finding different ways to solve a problem and
+    catch the makroperspektive, take an eye on datatraffic, load performance and a flow in UX. In the future I want to learn Phyton for AI and Machine Learning.
+    I truly want to understand things on a deeper level, that's the main reason why I feel so
+    comfertable within the Programming sphere.
+    We Programmer have to constantly adapt, but I am curious what smarter ways an tech people find to be more efficient and productive.
+    Nobody is good in every Language but we can learn from each other and make the digital life a good place to experience technology.`]:
+    [`Ich habe durch meine projektorientierte Arbeit ein gutes Fundament an Fähigkeiten in HTML, CSS und JavaScript geschaffen.
+    Ich habe einen Task-Manager, ein Canvas-Spiel und einige einfache Projekte erstellt, bei denen ich angefangen habe. Meine Erkenntnisse sind, verschiedene Möglichkeiten zu finden, um ein Problem zu lösen und die Makroperspektive einzunehmen, ein Auge auf den Datenverkehr, die Ladeleistung und den Fluss in der UX zu haben. In Zukunft möchte ich Python für KI und maschinelles Lernen lernen. 
+    Ich möchte Dinge wirklich auf einer tieferen Ebene verstehen, das ist der Hauptgrund, warum ich mich in der Programmierwelt so wohl fühle. 
+    Wir Programmierer müssen uns ständig anpassen, aber ich bin neugierig, welche intelligenteren Wege Menschen in der Technik finden, um effizienter und produktiver zu sein. 
+    Niemand ist in jeder Sprache gut, aber wir können voneinander lernen und das digitale Leben zu einem guten Ort machen, um Technologie zu erleben.`],
+
+    "Let's talk": "Lass uns reden",
+    "I have a special interest in learning:": "Ich habe ein besonderes Interesse diese Sprachen zu lernen:",
+    "Growth Mindset": "Wachstumsdenken",
+}
